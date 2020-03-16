d8a51f91c3b17481898df4df3357886ad5b5f370
“howard1994 <565449577@qq.com>
2020-03-16 15:33:07 +0800

    解决冲突

--- Xin.ExternalService.EC.WMSTests/Request/GetAsnListRequestTests.cs
+++ Xin.ExternalService.EC.WMSTests/Request/GetAsnListRequestTests.cs
@@ -14,21 +14,13 @@ namespace Xin.ExternalService.EC.WMS.Request.Tests
         [TestMethod()]
         public async Task RequestTest()
         {
-            try
-            {
-                var reqModel = new GetAsnListRequestModel();
-                 reqModel.page = 1;
-                reqModel.pageSize = 100;
-                reqModel.receivingCode = "RVA007-200118-0003";
-                GetAsnListRequest req = new GetAsnListRequest("7417441d04ea6267a57cbb6cdced5552", "726fb5fbe5b258d33e32aba78df42e83", reqModel);
-                var response = await req.Request();
+            var reqModel = new GetAsnListRequestModel();
+            reqModel.page = 1;
+            reqModel.pageSize = 100;
+            reqModel.receivingCode = "RVA007-200304-0001";
 
-            }
-            catch (Exception ex )
-            {
-
-                throw;
-            }
+            GetAsnListRequest req = new GetAsnListRequest("7417441d04ea6267a57cbb6cdced5552", "726fb5fbe5b258d33e32aba78df42e83", reqModel);
+            var response = await req.Request();
         }
     }
 }
