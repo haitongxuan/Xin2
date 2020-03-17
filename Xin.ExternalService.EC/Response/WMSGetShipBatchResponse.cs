8914bbb4f094642b66d385b5263f85e62dbd8633
howard1994 <565449577@qq.com>
2020-03-17 10:25:10 +0800

    添加头程出库单接口

---
+++ Xin.ExternalService.EC/Response/WMSGetShipBatchResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xin.ExternalService.EC.Response.Model;
+
+namespace Xin.ExternalService.EC.Response
+{
+   public class WMSGetShipBatchResponse :BaseResponse
+    {
+
+        public WMSGetShipBatchResponse(ECResponseBody body) : base(body)
+        {
+
+        }
+
+        public EC_ShipBatch Body { get; set; }
+    }
+}
