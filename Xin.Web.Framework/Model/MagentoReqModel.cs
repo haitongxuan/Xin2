31243207324706b1b724143bd54ca07ae063fc7b
howard1994 <565449577@qq.com>
2020-04-13 15:30:00 +0800

    magento物流接口

---
+++ Xin.Web.Framework/Model/MagentoReqModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xin.Web.Framework.Model
+{
+    public class MagentoReqModel
+    {
+        public string original_order_num { get; set; }
+        public string express_num { get; set; }
+        public string store { get; set; }
+    }
+}
